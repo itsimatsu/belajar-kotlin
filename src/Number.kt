a3c1402c99fcf081082abfd7176766cd8b0d1e30
USTAMIRAJIB <ustamirajib@student.telkomuniversity.ac.id>
2021-04-17 11:11:17 +0700

    add number

---
+++ src/Number.kt
@@ -0,0 +1,11 @@
+fun main(){
+    var age: Byte = 30;
+    var binary: Int = 0b101
+
+    //Underscore in number
+    var height: Int = 3_000;
+    var price: Long = 90000000L
+
+    println(price);
+    println(binary)
+}
