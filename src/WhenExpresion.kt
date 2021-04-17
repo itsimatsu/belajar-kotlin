cfe42ee1b095bdd6e59f4f4c9b4f771ff21791e1
USTAMIRAJIB <ustamirajib@student.telkomuniversity.ac.id>
2021-04-17 11:43:00 +0700

    when expresion kode

---
+++ src/WhenExpresion.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>){
+    val Nilai: Char = 'A'
+
+    when(Nilai){
+        'A' -> {
+            println("Good job")
+        }
+        'B' -> {
+            println("Never give up")
+        }
+        'C' -> {
+            println("U study?")
+        }
+    }
+
+}
