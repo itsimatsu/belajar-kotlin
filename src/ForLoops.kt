d601e1da0f6424d3f98e0699ba71c7f560a1229e
USTAMIRAJIB <ustamirajib@student.telkomuniversity.ac.id>
2021-04-17 10:39:12 +0700

    forloop kode

---
+++ src/ForLoops.kt
@@ -0,0 +1,17 @@
+fun main(){
+    val names = arrayOf("Ustami", "Rajib", "Javascript")
+    var length = 0
+    val ukuranArray = names.size - 1
+    for (name in names){
+        println(name)
+        length++
+    }
+    println("Length array = " + length)
+
+    for (i in 0..5){
+        println(i)
+    }
+    for (x in 0..ukuranArray){
+        println("Index ke $x =  ${names.get(x)}")
+    }
+}
